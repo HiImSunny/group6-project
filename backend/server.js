02f7b810592470808631d61a0cf81e3fff59f5aa
meankhoiii <khoiphan78757@gmail.com>
2025-10-12 22:43:15 +0700

    Tích hợp MongoDB Atlas vào backend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://meankhoiii:Minhkhoi78757@cluster0.kghqnri.mongodb.net/groupDB?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log('✅ Connected to MongoDB Atlas'))
+  .catch(err => console.error('❌ MongoDB connection error:', err));
+
+
 const express = require('express');
 const app = express();
 
@@ -6,8 +13,18 @@ const userController = require('./controllers/userController');
 
 app.use(express.json());
 
-const userRoutes = require('./routes/user');
-app.use('/api', userRoutes);
+const User = require('./models/User');
+
+app.get('/users', async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+app.post('/users', async (req, res) => {
+  const newUser = new User(req.body);
+  await newUser.save();
+  res.json(newUser);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server running on port', PORT));
